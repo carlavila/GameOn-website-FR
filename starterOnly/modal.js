ce40f17c03dfbe346485008a0b0047f724d020c3
carla <vila.carla@yahoo.fr>
2022-07-21 09:40:31 +0200

    modif modal.js

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,5 +1,5 @@
 function editNav() {
-  var x = document.getElementById("myTopnav");
+  let x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
   } else {
@@ -8,9 +8,26 @@ function editNav() {
 }
 
 // DOM Elements
+const topNav = document.getElementById("myTopnav");
 const modalbg = document.querySelector(".bground");
+document.getElementById("reserve").addEventListener("submit", function (e){
+  e.preventDefault();
+  alert ("formulaire envoyé!");
+});
+const content = document.querySelector(".content");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+
+
+// inputs
+const inputs = document.querySelectorAll("#firstname, #lastname, #email, #birthdate, #quantity");
+const firstname = document.getElementById("firstname");
+const lastname = document.getElementById("lastname");
+const email = document.getElementById("email");
+const birthdate = document.getElementById("birthdate");
+const quantity = document.getElementById("quantity");
+const locations = document.querySelectorAll("input[type=radio]");
+const checkbox1 = document.getElementById("checkbox1");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,5 +36,4 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
-
 
